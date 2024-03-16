287be50490a64e455c446525b8d7371aa58933de
Matheus Kamer <matheus_kamer@hotmail.com>
2024-03-16 19:08:06 -0400

    feat: finish Sidebar component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,5 @@
+import { PencilLine } from "@phosphor-icons/react";
+
 export function Sidebar() {
   return(
     <aside className="w-[256px] bg-gray-800 rounded-lg overflow-hidden">
@@ -6,17 +8,22 @@ export function Sidebar() {
         className="w-full h-[72px] object-cover"
       />
 
-      <div className="flex flex-col items-center">
-        <strong>Matheus Kamer</strong>
-        <span>FullStack Developer</span>
+      <div className="flex flex-col items-center mt-[-32px]">
+        <img
+          src="https://github.com/MatheusKamer.png"
+          className="w-16 h-16 rounded-lg outline outline-green-500 border-4 border-gray-800"
+        />
+        <strong className="mt-4 text-gray-100 leading-[160%]">Matheus Kamer</strong>
+        <span className="text-sm text-gray-400 leading-[160%]">FullStack Developer</span>
       </div>
 
       <footer className="flex justify-center border-t-[1px] border-gray-600 mt-6 p-8 pt-6">
         <a
           href="#"
-          className="w-full flex items-center justify-center bg-transparent text-green-500 border border-green-500 px-6 pt-4 pb-[14px] rounded-lg"
+          className="w-full flex items-center justify-center bg-transparent text-green-500 border border-green-500 px-6 pt-4 pb-[14px] rounded-lg gap-2 hover:bg-green-500 hover:text-white transition-all"
         >
-          <button>EDITAR PERFIL</button>
+          <PencilLine className="w-5 h-5"/>
+          <button>Editar seu perfil</button>
         </a>
 
       </footer>
