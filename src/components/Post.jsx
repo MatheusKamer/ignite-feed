0290cc8078982da26520a406583360712946d06f
Matheus Kamer <matheus_kamer@hotmail.com>
2024-03-20 21:17:35 -0400

    feat: add header of the Post Component

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,5 +1,21 @@
 export function Post() {
   return(
-    <h1>Post</h1>
+    <article>
+      <header>
+        <div>
+          <img
+            src="https://github.com/MatheusKamer.png"
+            className="w-16 h-16 rounded-lg outline outline-green-500 border-4 border-gray-800"
+          />
+
+          <div>
+            <strong>Matheus Kamer</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+
+        <time dateTime="">Publicado há 1h</time>
+      </header>
+    </article>
   )
 }
